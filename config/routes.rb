b53094dc663cd03ea8780f39ba67741a862eaba4
pbrudny <pbrudny@gmail.com>
2018-01-01 17:16:20 +0100

    feature: add coordinators inde

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :users, only: [:index, :edit, :update, :show, :destroy]
+
   resources :people do
     member do
       get 'help'
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20171230145240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "coordinators", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.string   "phone"
