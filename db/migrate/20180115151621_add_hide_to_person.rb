01bfc7e60049ab94c131887c6fddbc97e287e6ce
pbrudny <pbrudny@gmail.com>
2018-01-15 16:17:32 +0100

    add hide

---
+++ db/migrate/20180115151621_add_hide_to_person.rb
@@ -0,0 +1,5 @@
+class AddHideToPerson < ActiveRecord::Migration[5.0]
+  def change
+    add_column :people, :hide, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115144831) do
+ActiveRecord::Schema.define(version: 20180115151621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180115144831) do
     t.integer  "user_id"
     t.string   "age"
     t.boolean  "display"
+    t.boolean  "hide"
   end
 
   create_table "users", force: :cascade do |t|
