a1e36bd70d7fa05c0d95d9fc8eff134bb9d32ee5
pbrudny <pbrudny@gmail.com>
2018-01-16 00:41:11 +0100

    add translation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'user_destroyed' }
     end
   end
 
