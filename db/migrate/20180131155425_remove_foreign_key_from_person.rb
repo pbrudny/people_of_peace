df21b250fdc854da6cb4688d70430d4928e0f4c1
pbrudny <pbrudny@gmail.com>
2018-02-12 23:18:05 +0100

    fix: remove useless migration

--- db/migrate/20180131155425_remove_foreign_key_from_person.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignKeyFromPerson < ActiveRecord::Migration[5.0]
-  def change
-    remove_foreign_key :people, column: :coordinator_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131155425) do
+ActiveRecord::Schema.define(version: 20180212070537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "markers", force: :cascade do |t|
+    t.string   "need"
+    t.string   "color"
+    t.string   "letter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
