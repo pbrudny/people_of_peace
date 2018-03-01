f66c528fd67494cb5936e59ba2077ccf36eaf1d6
pbrudny <pbrudny@gmail.com>
2018-03-01 20:52:25 +0100

    remove sentry config

--- config/application.rb
+++ config/application.rb
@@ -14,12 +14,5 @@ module PeopleOfPeace
     config.i18n.default_locale = :pl
     config.paths.add File.join('app', 'services'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'services', '*')]
-
-    config.filter_parameters << :password
-
-    Raven.configure do |config|
-      config.dsn = ENV['SENTRY_ID']
-      config.environments = ['staging', 'production']
-    end
   end
 end
